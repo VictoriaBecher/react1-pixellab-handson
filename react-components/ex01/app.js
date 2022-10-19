dd0a070f0548e9508716dddcf4a8afd3ea8da2bd
VictoriaBecher <victoria_enache@ymail.com>
2022-10-19 21:56:43 +0300

    map reduce

--- react-components/ex01/app.js
+++ react-components/ex01/app.js
@@ -31,3 +31,47 @@ const person = {
     },
   ],
 };
+
+console.warn(
+  `Folosind metoda map pe arrayul skills, returneaza si afiseaza in consola un array in care fiecare consoana este scrisa cu litera mare (vocalele nu)`,
+);
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const arr1 = person.skills.map((skill) => {
+  const letters = skill.split('');
+  letters.forEach((letter, index, letters) => {
+    if (!vowels.includes(letter)) {
+      letters[index] = letters[index].toLocaleUpperCase();
+    }
+  });
+
+  return letters.join('');
+});
+
+console.log(arr1);
+
+console.warn(`Folosind map pe arrayul friends, returneaza un array in care fiecare pozitie contine propozitia
+“Ma numesc {name} {surname} si am {age} ani.`);
+const arr2 = person.friends.map(({ name, surname, age }) => {
+  return `Ma numesc ${name} ${surname} si am ${age} ani.`;
+});
+console.log(arr2);
+
+console.warn(`Folosind map pe arrayul friends, returneaza un array in care fiecare pozitie contine propozitia
+“Diferenta de varsa dintre {friendName} si {personName} este {diff}”`);
+const arr3 = person.friends.map(({ name, age }) => {
+  const ageDiff = Math.abs(person.age - age);
+  return `Diferenta de varsta dintre ${person.name} si ${name} este ${ageDiff} ani`;
+});
+console.log(arr3);
+
+console.warn(`Returneaza si afiseaza un array in care
+fiecare pozitie contine diferenta
+dintre varsta persoanei si lungimea cuvantului de pe arrayul skill`);
+const arr4 = person.skills.map((skill) => {
+  const skillLength = skill.length;
+
+  return Math.abs(person.age - skillLength);
+});
+console.log(arr4);
--- react-components/ex02/app.js
+++ react-components/ex02/app.js
@@ -31,3 +31,76 @@ const person = {
     },
   ],
 };
+const dropChars = (string, charCount = 2) => {
+  return string.slice(0, string.length - charCount);
+};
+console.warn(
+  `Folosind obiectul person si reduce, afiseaza in consola un string care contine skillurile de pe pozitiile pare ale arrayului, separate prin virgula`,
+);
+const string1 = person.skills.reduce((string1, skill, index, skills) => {
+  if (index % 2 === 0) {
+    string1 += `${skill}, `;
+  }
+  return string1;
+}, '');
+console.log(dropChars(string1));
+
+console.warn(' In mod similar, afiseaza skillurile care NU incep cu j.');
+const string2 = person.skills.reduce((string2, skill) => {
+  if (!skill.startsWith('j')) {
+    string2 += `${skill}, `;
+  }
+
+  return string2;
+}, '');
+console.log(dropChars(string2));
+
+console.warn(
+  ` Folosind reduce afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy, xxx yyy."`,
+);
+const sentence = person.friends.reduce(
+  (sentence, { name, surname }, index, friends) => {
+    const punctuation = index === friends.length - 1 ? '.' : ', ';
+
+    sentence += `${name} ${surname} ${punctuation}`;
+    return sentence;
+  },
+  'Prietenii mei se numesc ',
+);
+console.log(sentence);
+
+console.warn(
+  ` Folosind reduce, afiseaza numarul total de ani pe care il au persoanele din arrayul friends, doar daca varsta este mai mare sau egala cu 30 de ani.`,
+);
+const sumYears = person.friends.reduce((sumYears, { age }) => {
+  if (age >= 30) {
+    sumYears += age;
+  }
+  return sumYears;
+}, 0);
+console.log(sumYears);
+
+console.warn(
+  ` Folosind reduce, afiseaza suma anilor de nastere ai persoanelor. `,
+);
+const sumBirthYears = person.friends.reduce((sumBirthYears, { age }) => {
+  const currentYear = new Date().getFullYear();
+
+  sumBirthYears += currentYear - age;
+
+  return sumBirthYears;
+}, 0);
+console.log(sumBirthYears);
+
+console.warn(
+  ` Afiseaza fraza: "Intre Dragos si Larry este o diferenta de xx ani. Intre Dragos si Steven... ", doar daca varsta prietenului este impara. `,
+);
+const string3 = person.friends
+  .reduce((string3, { age, name }) => {
+    string3 += `intre ${person.name} si ${name} este o diferenta de ${Math.abs(
+      person.age - age,
+    )} ani. `;
+    return string3;
+  }, '')
+  .trim();
+console.log(string3);
